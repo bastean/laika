1fd4068c6e256780b11df8c9532c94a1bf8ae6fa
Bastean <bastean@pm.me>
2024-03-04 20:56:17 -0300

    feat(context): upgrade plain email regexp

--- pkg/context/shared/domain/service/regexp.go
+++ pkg/context/shared/domain/service/regexp.go
@@ -2,7 +2,7 @@ package service
 
 import "regexp"
 
-var PlainEmailRegexp = regexp.MustCompile(`[\w\d.+~-]+@[A-Za-z-\d]+\.[a-z]{2,4}`)
+var PlainEmailRegexp = regexp.MustCompile(`[\w\d.+~-]+@[A-Za-z-\d]+\.[a-z]{2,4}(\.[a-z]{2,4})?`)
 
 var HtmlCommentRegexp = regexp.MustCompile(`<!--.*?-->`)
 
